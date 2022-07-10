9b97a64c79187c8e64e772441a5680e6bb7ddb79
Ahmed <ahmed@hallajian.co.uk>
2022-07-10 10:34:29 +0100

    Test drove some CRUD routes

--- app.rb
+++ app.rb
@@ -1,16 +1,57 @@
 # file: app.rb
-require 'sinatra'
+require "sinatra"
 require "sinatra/reloader"
-require_relative 'lib/database_connection'
-require_relative 'lib/album_repository'
-require_relative 'lib/artist_repository'
+require_relative "lib/database_connection"
+require_relative "lib/album_repository"
+require_relative "lib/artist_repository"
 
 DatabaseConnection.connect
 
 class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
-    also_reload 'lib/album_repository'
-    also_reload 'lib/artist_repository'
+    also_reload "lib/album_repository"
+    also_reload "lib/artist_repository"
+  end
+
+  get "/albums" do
+    repo = AlbumRepository.new
+    albums = repo.all
+
+    response = albums.map do |album|
+      album.title
+    end.join(", ")
+
+    return response
+  end
+
+  post "/albums" do
+    title = params[:title]
+    release_year = params[:release_year]
+    artist_id = params[:artist_id]
+
+    new_album = Album.new
+    new_album.title = title
+    new_album.release_year = release_year
+    new_album.artist_id = artist_id
+    
+    repo = AlbumRepository.new
+    repo.create(new_album)
+  end
+
+  get "/artists" do
+    repo = ArtistRepository.new
+    artists = repo.all
+
+    artists.map {|artist| artist.name}.join(", ")
+  end
+
+  post "/artists" do
+    artist = Artist.new
+    artist.name = params[:name]
+    artist.genre = params[:genre]
+
+    repo = ArtistRepository.new
+    repo.create(artist)
   end
 end
--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -1,8 +1,18 @@
 require "spec_helper"
 require "rack/test"
-require_relative '../../app'
+require_relative "../../app"
 
 describe Application do
+  def reset_tables
+    seed_sql = File.read("spec/seeds/music_library.sql")
+    connection = PG.connect({ host: "127.0.0.1", dbname: "music_library_test" })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do
+    reset_tables
+  end
+
   # This is so we can use rack-test helper methods.
   include Rack::Test::Methods
 
@@ -10,4 +20,32 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context "POST to /albums" do
+    it "creates new album and includes it when getting list of all albums" do
+      response = post("/albums", title: "Voyage", release_year: 2022, artist_id: 2)
+      album_list = get("albums").body
+
+      expect(response.status).to eq 200
+      expect(album_list[-6, 6]).to eq "Voyage"
+    end
+  end
+
+  context "GET to /artists" do
+    it "returns a comma separated list of all the artists" do
+      response = get("/artists")
+
+      expect(response.status).to eq 200
+      expect(response.body).to eq "Pixies, ABBA, Taylor Swift, Nina Simone"
+    end
+  end
+
+  context "POST /artists" do
+    it "creates a new artists in the database" do
+      response = post("/artists", name: "Wild nothing", genre: "Indie")
+      artists = get("/artists").body
+
+      expect(response.status).to eq 200
+      expect(artists).to eq "Pixies, ABBA, Taylor Swift, Nina Simone, Wild nothing"
+    end
+  end
 end
